e32395b3c95fcc8b0bc6fccebeaf617cec12d151
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2023-01-16 20:53:05 +0200

    added mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+const { default: mongoose } = require('mongoose');
 const app = require('./app');
 
 
 const start = async () => {
+    try {
+
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('connected');
+    } catch(err) {
+        console.log(err);
+    }
+
     app.listen(3000, () => {
         console.log('app is listening on port 3000');
     })
+
 }
 
 start();
--- src/medels/User.js
+++ src/medels/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 
 const userSchema = new mongoose.Schema({
@@ -19,12 +20,49 @@ const userSchema = new mongoose.Schema({
         required: [true, 'You must provide a password'],
         min: [6, 'The minimum length of the password is 6']
     },
+    passwordConfirmed: {
+        type: String,
+        required: true,
+        validate: {
+            // ONLY WORKS WITH CREATE/SAVE
+            validator: function(el) {
+                return el === this.password;
+            }
+        }
+    },
+    address: String,
+    phone: String,
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
     role: {
         type: String,
         enum: ['admin', 'manager', 'employee', 'user'],
         default: 'user'
     }
-})
+});
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password') || this.isNew) {
+        return next();
+    }
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirmed = undefined;
+    next();
+
+});
+
+userSchema.methods.isPasswordCorrect = async function(candidatePassword, currentPassword) {
+    return await bcrypt.compare(candidatePassword, currentPassword);
+}
 
 
 module.exports = mongoose.model('User', userSchema);
